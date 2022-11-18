89bd3f82f3ac79916d03e40eccf8becf68e1625f
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-11-18 18:52:31 +0100

    Ajout de nouveaux tests pour les mÃéthodes setAddUserEltCallback et createHtmlUser

--- tests/users-ui.test.js
+++ tests/users-ui.test.js
@@ -15,6 +15,33 @@ describe("createHtmlUser", () => {
   test("returned element must have a CSS class 'user'", () => {
     expect(createHtmlUser(12).classList.contains("user")).toBe(true);
   });
+
+  test("returned element must contain an element input[type='text'].input.user__name", () => {
+    expect(
+      createHtmlUser(12).querySelector(
+        "input[type='text'].input.user__name, input.input.user__name",
+      ),
+    ).not.toBe(null);
+  });
+
+  test("returned element must contain an element input[type='number'].input.user__age", () => {
+    expect(
+      createHtmlUser(12).querySelector("input[type='number'].input.user__age"),
+    ).not.toBe(null);
+  });
+
+  test("returned element must contain an element button[type='button'].button.user__delete", () => {
+    expect(
+      createHtmlUser(12).querySelector("input[type='number'].input.user__age"),
+    ).not.toBe(null);
+  });
+
+  test("input[type='number'].input.user__age must contain given parameter", () => {
+    expect(
+      createHtmlUser(12).querySelector("input[type='number'].input.user__age")
+        .value,
+    ).toBe("12");
+  });
 });
 
 describe("setAddUserEltCallback", () => {
