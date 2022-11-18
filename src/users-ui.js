b86e6d12d4ea842e10f8a7c8f61b85b819d158a9
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-11-18 18:52:12 +0100

    Modification dees fonctions createHtmlUser et setAddUserEltCallback

--- src/users-ui.js
+++ src/users-ui.js
@@ -1,12 +1,15 @@
-export function createHtmlUser() {
+export function createHtmlUser(age) {
   const li = document.createElement("li");
   li.className = "user";
+  li.innerHTML +=  "<input type='text' value='Name' class='input user__name'>";
+  li.innerHTML += `<input type='number' value='${  age }' class='input user__age'>`;
+  li.innerHTML += "<input type='button' value='delete' class='input user__delete'>";
   return li;
 }
 
 export function setAddUserEltCallback(addUserElt, usersElt) {
-
-  document.getElementsByClassName(addUserElt)[0].addEventListener('click', () => {
-    document.getElementsByClassName(usersElt)[0].appendChild(createHtmlUser())
+  addUserElt.addEventListener('click', () => {
+    const age = Math.floor(Math.random() * 42);
+    usersElt.appendChild(createHtmlUser(age))
   });
 }
