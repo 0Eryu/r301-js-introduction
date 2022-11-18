8506b416ed146fe1755971d78998341bf54ff216
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-11-18 14:36:09 +0100

    Ajout du test de la fonction addToAll

--- tests/introduction.test.js
+++ tests/introduction.test.js
@@ -1,4 +1,4 @@
-import { add, sub, sum } from "../src/introduction";
+import { add, addToAll, sub, sum } from "../src/introduction";
 
 test("add 1 to 2 equals 3", () => {
   expect(add(1, 2)).toBe(3);
@@ -14,5 +14,15 @@ describe("sum", () => {
   });
   test("sum of an empty array equals 0", () => {
     expect(sum([])).toBe(0);
+  });
+});
+
+describe("addToAll", () => {
+  test("add 2 to all : 1, 2 and 3 equals 3, 4, 5", () => {
+    expect(addToAll([1, 2, 3], 2)).toEqual([3, 4, 5]);
+  });
+  test("return a new array", () => {
+    const values = [1, 2];
+    expect(addToAll(values)).not.toBe(values);
   });
 });
