29074cd95886bdd81593f9ddf13f0bdd1e1bc6c5
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-11-18 14:28:51 +0100

    Ajout du test de la fonction sum dans le fichier introduction.test.js

--- tests/introduction.test.js
+++ tests/introduction.test.js
@@ -1,4 +1,4 @@
-import { add, sub } from "../src/introduction";
+import { add, sub, sum } from "../src/introduction";
 
 test("add 1 to 2 equals 3", () => {
   expect(add(1, 2)).toBe(3);
@@ -6,4 +6,13 @@ test("add 1 to 2 equals 3", () => {
 
 test("sub 1 to 2 equals 1", () => {
   expect(sub(1, 2)).toBe(1);
+});
+
+describe("sum", () => {
+  test("sum of 1, 2 and 3 equals 6", () => {
+    expect(sum([1, 2, 3])).toBe(6);
+  });
+  test("sum of an empty array equals 0", () => {
+    expect(sum([])).toBe(0);
+  });
 });
