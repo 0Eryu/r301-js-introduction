3c4f6781b6c3b3562e43b80b760dabac322c0978
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-11-18 14:05:01 +0100

    Modification de la configuration d'ESLint dans le fichier .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
+  plugins: ["prettier", "jest"],
   rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        singleQuote: false,
+        trailingComma: "all",
+        tabWidth: 2,
+      },
+    ],
   },
 };
