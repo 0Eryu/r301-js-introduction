9fd132f774ad3b00e792cb9c6316b5511886a3f7
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-11-18 18:32:23 +0100

    Ajout de l la fonction setAddUserEltCallback

--- src/users-ui.js
+++ src/users-ui.js
@@ -3,3 +3,10 @@ export function createHtmlUser() {
   li.className = "user";
   return li;
 }
+
+export function setAddUserEltCallback(addUserElt, usersElt) {
+
+  document.getElementsByClassName(addUserElt)[0].addEventListener('click', () => {
+    document.getElementsByClassName(usersElt)[0].appendChild(createHtmlUser())
+  });
+}
