a0f93051c2d76e23095d515bebf1de02aa1cb305
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-11-21 21:05:27 +0100

    Modification de la fonction setAddUserEltCallback pour rendre le paramètre infoElt optionnel

--- src/users-ui.js
+++ src/users-ui.js
@@ -3,29 +3,34 @@ import { ageAverage } from "./introduction";
 export function createHtmlUser(age) {
   const li = document.createElement("li");
   li.className = "user";
-  li.innerHTML +=  "<input type='text' value='Name' class='input user__name'>";
-  li.innerHTML += `<input type='number' value='${  age }' class='input user__age'>`;
-  li.innerHTML += "<input type='button' value='delete' class='input user__delete'>";
+  li.innerHTML += "<input type='text' value='Name' class='input user__name'>";
+  li.innerHTML += `<input type='number' value='${age}' class='input user__age'>`;
+  li.innerHTML +=
+    "<input type='button' value='delete' class='input user__delete'>";
   return li;
 }
 
-export function setAddUserEltCallback(addUserElt, usersElt) {
-  addUserElt.addEventListener('click', () => {
+export function setAddUserEltCallback(addUserElt, usersElt, infoElt) {
+  // eslint-disable-next-line no-param-reassign
+  infoElt = infoElt || null;
+  addUserElt.addEventListener("click", () => {
     const age = Math.floor(Math.random() * (32 - 12 + 1) + 12);
-    usersElt.appendChild(createHtmlUser(age))
-    // eslint-disable-next-line no-use-before-define
-    updateAgeAverage(usersElt, document.querySelector("article.info"));
+    usersElt.appendChild(createHtmlUser(age));
+    if (infoElt !== null) {
+      // eslint-disable-next-line no-use-before-define
+      updateAgeAverage(usersElt, infoElt);
+    }
   });
 }
 
 export function extractUser(userElt) {
-  const name = userElt.getElementsByClassName('user__name')[0].value;
-  const age = userElt.getElementsByClassName('user__age ')[0].value;
-  return {name, age: parseInt(age, 10)};
+  const name = userElt.getElementsByClassName("user__name")[0].value;
+  const age = userElt.getElementsByClassName("user__age ")[0].value;
+  return { name, age: parseInt(age, 10) };
 }
 
 export function extractUsers(usersElt) {
-  const users = Array.from(usersElt.querySelectorAll('li.user'));
+  const users = Array.from(usersElt.querySelectorAll("li.user"));
   const returnUsers = [];
   // eslint-disable-next-line no-restricted-syntax
   for (const user of users) {
@@ -47,11 +52,10 @@ export function extractUserType(infoElt) {
 }
 
 export function updateAgeAverage(usersElt, infoElt) {
-  const average = ageAverage(extractUsers(usersElt),  extractUserType(infoElt));
-  const HTMLElt = infoElt.querySelector('span.info__age-average');
+  const average = ageAverage(extractUsers(usersElt), extractUserType(infoElt));
+  const HTMLElt = infoElt.querySelector("span.info__age-average");
   HTMLElt.innerText = "";
   if (average !== null) {
     HTMLElt.innerText = parseFloat(average).toFixed(2);
   }
-
 }
